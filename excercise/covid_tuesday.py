43e6fd06ec0754e780589f98eb4253a13e3f3aad
rdlaura <63103200+rdlaura@users.noreply.github.com>
2021-02-09 17:39:07 +0100

    data set now clean and pretty yaaaay

--- excercise/covid_tuesday.py
+++ excercise/covid_tuesday.py
@@ -15,3 +15,48 @@ cdf = pd.DataFrame(covid_json['records'])
 
 # print(cdf.sample(10))
 
+# rename columns
+print(cdf.info())
+cdf.rename(
+    columns={
+        "dateRep": "rep_date",
+        "countriesAndTerritories": "region",
+        "geoId": "geo_id",
+        "countryterritoryCode": "region_code",
+        "popData2019": "pop_2019",
+        "continentExp": "continent",
+        "notification_rate_per_100000_population_14-days": "14d_incidence"
+    },
+    inplace=True
+)
+
+# get the types straight
+cdf['date_reported'] = pd.to_datetime(cdf['rep_date'])
+cdf['date_reported'].dt.day.head()
+
+cdf["14d_incidence"] = pd.to_numeric(cdf["14d_incidence"])  # convert 14 day incidence into float64
+
+# add new column for delta time --> delta time since start of recording
+# find start of recording
+cdf.date_reported.idxmin()  # Out: Timestamp('2020-01-06 00:00:00')
+strg_date = cdf.date_reported[35]
+
+cdf["delta_t"] = cdf.date_reported - strg_date
+
+# spot shit and fix it
+cdf.dropna(inplace=True)  # 10433 to 10195
+
+# cdf = cdf[cdf.cases_weekly >= 0 and cdf.deaths_weekly >= 0 and cdf.pop_2019 >= 0 and cdf["14d_incidence"] >= 0]
+cdf = cdf[cdf.cases_weekly >= 0]
+cdf = cdf[cdf.deaths_weekly >= 0]
+cdf = cdf[cdf.pop_2019 >= 0]
+cdf = cdf[cdf["14d_incidence"] >= 0]
+
+# drop cdf.cases_weekly --> 10187
+# drop deaths_weekly --> 10185
+# drop pop_2019 --> 10185
+# drop 14d_incidence --> 10182
+# summed up: dropped 251 rows
+
+# group
+
