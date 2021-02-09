fb7da05a7fcb5ce004d667492e10d34e4b58fba0
rdlaura <63103200+rdlaura@users.noreply.github.com>
2021-02-09 15:17:38 +0100

    doc and comment

--- excercise/covid_tuesday.py
+++ excercise/covid_tuesday.py
@@ -3,7 +3,8 @@ import ssl
 import json
 import urllib
 
-# retrieve
+# retrieve covid data from ecdc
+
 covid_url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -12,4 +13,5 @@ covid_json_unformated = urllib.request.urlopen(covid_url).read().decode("utf-8")
 covid_json = json.loads(covid_json_unformated)
 cdf = pd.DataFrame(covid_json['records'])
 
-print(cdf.sample(10))
+# print(cdf.sample(10))
+
