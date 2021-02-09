0debd3b322f02de6670cb9e195a8b912a907d756
rdlaura <63103200+rdlaura@users.noreply.github.com>
2021-02-09 15:15:28 +0100

    imported covid data from ecdc using the provided code

---
+++ excercise/covid_tuesday.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import ssl
+import json
+import urllib
+
+# retrieve
+covid_url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_json_unformated = urllib.request.urlopen(covid_url).read().decode("utf-8")
+covid_json = json.loads(covid_json_unformated)
+cdf = pd.DataFrame(covid_json['records'])
+
+print(cdf.sample(10))
