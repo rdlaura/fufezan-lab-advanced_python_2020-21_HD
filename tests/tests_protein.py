7d1845789340599aa81dddc083b226b14e466d89
rdlaura <63103200+rdlaura@users.noreply.github.com>
2021-02-26 17:36:19 +0100

    set up test structure to protein class

---
+++ tests/tests_protein.py
@@ -0,0 +1,23 @@
+import needcoffee.protein as tf  # test folder
+from needcoffee.protein import protein as tc  # test class
+
+import pytest
+import pandas as pd
+import plotly.graph_objects as go
+
+def test_get_lookup_dict():
+    assert
+
+# def test_get_data():
+#     assert
+#
+# def test_map():
+#     assert
+#
+# def test_map_sld_wdw():
+#     assert
+#
+# def test_plot():
+#     assert
+
+# pytest --cov-report html --cov=needcoffee tests/
